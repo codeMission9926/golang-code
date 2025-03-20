d6c77c92ba14e245e9b541abd9b2af20a790ed96
codeMission9926 <yogeshrautela101@gmail.com>
2025-03-20 16:59:51 +0530

    pending

--- lesson_1/invesmentCalc.go
+++ lesson_1/invesmentCalc.go
@@ -7,37 +7,37 @@ import (
 
 // creating basic main func: To understand variables and types
 
-func main () {
+// func main () {
 	 
-	// defining variables
-     var investmentAmount float64 = 2000  
-	 var years float64 =5
-	 var expectedReturnRate float64 = 8.5
+// 	// defining variables
+//      var investmentAmount float64 = 2000  
+// 	 var years float64 =5
+// 	 var expectedReturnRate float64 = 8.5
 
-	 calc fiin () {	 
+// 	 calc fiin () {	 
 	
-	var Number1 float64 
-	var Number2 float64
+// 	var Number1 float64 
+// 	var Number2 float64
 
-	fmt.Print("Number1: ")  // Asking user to put a value
-	fmt.Scan(&Number1)        // taking value from user using scan func
+// 	fmt.Print("Number1: ")  // Asking user to put a value
+// 	fmt.Scan(&Number1)        // taking value from user using scan func
 
-	fmt.Print("Number2: ")  // Asking user to put a value
-	fmt.Scan(&Number2)  
+// 	fmt.Print("Number2: ")  // Asking user to put a value
+// 	fmt.Scan(&Number2)  
 
-	var addition = Number1 + Number2
+// 	var addition = Number1 + Number2
 
-	fmt.Println(addition)
-
-
-} final Amount
-	 var finalAmount = investmentAmount*math.Pow((1+(expectedReturnRate)/100), years)
-
-    // printing final Amount
-	 fmt.Println(finalAmount)
+// 	fmt.Println(addition)
 
 
- }
+// } final Amount
+// 	 var finalAmount = investmentAmount*math.Pow((1+(expectedReturnRate)/100), years)
+
+//     // printing final Amount
+// 	 fmt.Println(finalAmount)
+
+
+//  }
 
 
 // shorthand
@@ -81,12 +81,12 @@ func main () {
 
 
 
-// func ma
+func ma
 
 
-// package main 
+package main 
 
-// import "fmt"
+import "fmt"
 
-// func addition 
+func addition 
 
